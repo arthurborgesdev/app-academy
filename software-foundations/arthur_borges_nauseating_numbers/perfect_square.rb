9c49c10d6930184c1934d1ba3164f6a0e4debe61
Arthur Borges <arthurborges.dev@gmail.com>
2020-12-01 14:41:30 -0300

    Solve perfect_square

---
+++ software-foundations/arthur_borges_nauseating_numbers/perfect_square.rb
@@ -0,0 +1,16 @@
+def perfect_square(num)
+  (1..num).each do |n| 
+    return true if n * n == num
+  end
+  false
+end
+
+p perfect_square(1)     # true
+p perfect_square(4)     # true
+p perfect_square(64)    # true
+p perfect_square(100)   # true
+p perfect_square(169)   # true
+p perfect_square(2)     # false
+p perfect_square(40)    # false
+p perfect_square(32)    # false
+p perfect_square(50)    # false
