c0dcfb9efa0023d8d163b107eab15020941c3e99
Arthur Borges <arthurborges.dev@gmail.com>
2020-12-02 11:13:25 -0300

    Solve squaragonal

--- software-foundations/arthur_borges_nauseating_numbers/squaragonal.rb
+++ software-foundations/arthur_borges_nauseating_numbers/squaragonal.rb
@@ -1,4 +1,35 @@
 def squaragonal?(array)
+  i = 0
+  j = 0
+  size = array[0].length
+  results = []
+  compare_arr = []
+  while i < size
+    while j < size
+      compare_arr << array[i][j] if i = j
+      j += 1
+    end
+    j = 0
+    i += 1
+  end
+  i = 0
+  j = 0
+  # p "1st_diag: #{compare_arr}"
+  results << true if compare_arr.uniq.size <= 1
+  compare_arr = []
+  while i < size
+    while j < size
+      # p "i + j == size : #{i} + #{j} == #{size}"
+      compare_arr << array[i][j] if i + j == size - 1
+      j += 1
+    end
+    j = 0
+    i += 1
+  end
+  # p compare_arr
+  results << true if compare_arr.uniq.size == 1
+  # p results
+  results.any?
 end
 
 p squaragonal?([
