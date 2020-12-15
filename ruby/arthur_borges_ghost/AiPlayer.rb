13fdace64cacbbd3b0b4735d9b7cf5573a56b207
Arthur Borges <arthurborges.dev@gmail.com>
2020-12-15 18:03:11 -0300

    Working on Tree filtering and processing of AiPlayer class

--- ruby/arthur_borges_ghost/AiPlayer.rb
+++ ruby/arthur_borges_ghost/AiPlayer.rb
@@ -16,25 +16,35 @@ class AiPlayer
     winning_move = []
     possible_moves = []
     puts "#{name} is going to make a smart move..."
+    tree = {}
     ("a".."z").each do |letter|
       future_guess = fragment + letter
       # p future_guess
       possible_moves = @dictionary.select do |word|
         word.index(future_guess) == 0
       end 
-      compute_tree(possible_moves)
-      puts "possible_moves: #{possible_moves}"
-      smart_move = possible_moves.any? { |word| word.length - future_guess.length <= @n }
-      p "Smart_move: #{smart_move}"
-      if @dictionary.include?(future_guess) 
-        losing_move << letter
-      elsif smart_move
-        winning_move << letter
+      tree[letter] = {
+        branch: possible_moves,
+        losses: 0,
+        wins: 0
+      }
+      puts "possible_moves: #{tree[letter][:branch]}"
+      wins = tree[letter][:branch].count { |word| word.length - future_guess.length <= @n }
+      tree[letter][:wins] += wins
+      p "Wins: #{wins}"
+      if tree[letter][:branch].include?(future_guess) 
+        tree[letter][:losses] += 1
       end
-      puts "Winning_moves: #{winning_move}"
-      puts "losing_moves: #{losing_move}"
+      puts "Tree: #{tree}"
     end
+    calc_balance = {}
+    tree.each { |letter, value| calc_balance[letter] = value[:wins] - value[:losses] }
+    p calc_balance
 
+
+
+
+=begin
     unless winning_move.empty?
       random_win = rand(winning_move.size)
       return winning_move[random_win] 
@@ -42,14 +52,7 @@ class AiPlayer
       random_lose = rand(losing_move.size)
       return losing_move[random_lose]
     end
+=end
   end
 
-  def compute_tree(moves)
-    tree = { 
-      branch = Array.new {  }
-        
-      ]
-         
-
-  end
 end
