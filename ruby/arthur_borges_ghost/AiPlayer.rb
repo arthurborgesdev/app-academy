5d54b6af3ecd56f21b84607722a2151259e35ba3
Arthur Borges <arthurborges.dev@gmail.com>
2020-12-15 17:30:41 -0300

    Working version before major update of Ai Tree computing

--- ruby/arthur_borges_ghost/AiPlayer.rb
+++ ruby/arthur_borges_ghost/AiPlayer.rb
@@ -9,22 +9,21 @@ class AiPlayer
     @dictionary = dictionary
     @losing_move = []
     @winning_move = []
-    # @count = 0
   end
 
   def guess(fragment)
-    # @count = 0
     losing_move = []
     winning_move = []
     possible_moves = []
     puts "#{name} is going to make a smart move..."
     ("a".."z").each do |letter|
-      # p letter
       future_guess = fragment + letter
       # p future_guess
       possible_moves = @dictionary.select do |word|
         word.index(future_guess) == 0
       end 
+      compute_tree(possible_moves)
+      puts "possible_moves: #{possible_moves}"
       smart_move = possible_moves.any? { |word| word.length - future_guess.length <= @n }
       p "Smart_move: #{smart_move}"
       if @dictionary.include?(future_guess) 
@@ -43,5 +42,14 @@ class AiPlayer
       random_lose = rand(losing_move.size)
       return losing_move[random_lose]
     end
+  end
+
+  def compute_tree(moves)
+    tree = { 
+      branch = Array.new {  }
+        
+      ]
+         
+
   end
 end
