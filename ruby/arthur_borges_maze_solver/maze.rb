ffaac70001f0fe0df0a945954520290525c74e2c
Arthur Borges <arthurborges.dev@gmail.com>
2020-12-16 09:21:57 -0300

    Working on the size of the maze array

--- ruby/arthur_borges_maze_solver/maze.rb
+++ ruby/arthur_borges_maze_solver/maze.rb
@@ -8,5 +8,22 @@ class Maze
   end
 end
 
-maze = Maze.new
-p maze.maze_text
+# maze = Maze.new
+# p maze.maze_text
+p "aiushdui111"
+size = 0
+File.open("maze1.txt").each do |line|
+  size = line.chomp.length
+  break
+end
+maze_array = Array.new(size) { Array.new(size) }
+i = 0
+File.open("maze1.txt").each_with_index do |line, j|
+  while i < line.chomp.length 
+    maze_array[j][i] = line.chomp.split('')[i]
+    i += 1
+  end
+  i = 0 
+end
+
+p maze_array
