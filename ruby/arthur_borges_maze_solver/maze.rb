f7917270b93234bcb3bbfa5c423d64eaf99935b8
Arthur Borges <arthurborges.dev@gmail.com>
2020-12-18 14:39:10 -0300

    Refactored and restructured data structure to enclose the hashes of each node

--- ruby/arthur_borges_maze_solver/maze.rb
+++ ruby/arthur_borges_maze_solver/maze.rb
@@ -2,20 +2,20 @@ class Maze
   attr_reader :maze_text, :start_position, :end_position
   def initialize
     create_maze("maze1.txt")
-    @start_position = position(@maze_array, "S")
-    @end_position = position(@maze_array, "E")
+    @start_position = position(@maze, "S")
+    @end_position = position(@maze, "E")
     @current_position = start_position
     @open_list = []
     @closed_list = []
   end
 
   def create_maze(file)
-    @maze_array = []
+    @maze = []
     i = 0
     File.open("maze1.txt").each_with_index do |line, j|
-      @maze_array << []
+      @maze << []
       while i < line.chomp.length 
-        @maze_array[j] << line.chomp.split('')[i]
+        @maze[j] << line.chomp.split('')[i]
         i += 1
       end
       i = 0
@@ -23,40 +23,59 @@ class Maze
   end
 
   def position(maze, point)
-    @maze_array.each_with_index do |line, i|
+    @maze.each_with_index do |line, i|
       line.each_with_index do |elem, j|
         return([i, j]) if elem == point  
       end
     end
   end
 
-  def H_calc
-    (self.point[0] - @end_position[0]).abs + (self.point[0] - @end_position[1]).abs - 1
+  def H_calc(point)
+    (point[0] - @end_position[0]).abs + (point[1] - @end_position[1]).abs - 1
   end
 
   def print
-    @maze_array.each { |line| p line.join }
+    @maze.each { |line| p line.join }
   end
 
-  def adjacents(point, parent)
+  def adjacents(point, parent = nil)
     i = point[0] - 1
     j = point[1] - 1
-    while i < point[0] + 1
-      while j < point[1] + 1
-        next if point[0] + point[1] == i + j || (point[0] - point[1]).abs == (i - j).abs
-        unless point[i][j] == "*" || open_list.include?(point[i][j])
-          point_G_calc = parent.G + 10
-          point_H_calc = point.H_calc
-          @open_list.push(point[i][j]: { G: G_calc, H: point_H_calc, F: point_G_calc + point_H_calc } )  
+    while i <= point[0] + 1
+      while j <= point[1] + 1
+        p "i: #{i}"
+        p "j: #{j}"
+        p "@maze#{[i]}#{[j]}: #{@maze[i][j]}"
+        # p "point[0]: #{point[0]}"
+        # p "point[1]: #{point[1]}"
+        # p point[0] + point[1] == i + j
+        # p (point[0] - point[1]).abs == (i - j).abs
+        # p @maze[i][j] == "*"
+        # p @open_list.include?(point[i, j])
+        unless point[0] + point[1] == i + j || # second diagonal
+          (point[0] - point[1]).abs == (i - j).abs || # first diagonal
+          @maze[i][j] == "*" ||
+          @open_list.include?(point[i, j])
+          parent ? point_G_calc = parent.G + 10 : point_G_calc = 0
+          point_H_calc = H_calc(point)
+          hash_point = { 
+            point: [i, j], 
+            G: point_G_calc, 
+            H: point_H_calc, 
+            F: point_G_calc + point_H_calc 
+          }
+          @open_list << hash_point
+          p @open_list
         end
         j += 1
       end
+      j = 0
       i += 1
     end
   end
 
   def valid_adjacents(adjacent_positions)
-    valids = adjacent_positions.select! { |point| @maze_array[point[0]][point[1]] == " " }
+    valids = adjacent_positions.select! { |point| @maze[point[0]][point[1]] == " " }
   end
 
   def random_move(valid_adjacents)
@@ -72,27 +91,35 @@ class Maze
 
   def end_game?(position)
     puts "position: #{position}"
-    puts "@maze_array: #{@maze_array}"
+    puts "@maze: #{@maze}"
     # position_first = position.first
-    @maze_array[position[0]][position[1]] == "E"
+    @maze[position[0]][position[1]] == "E"
   end
 
   def run
-    # p @maze_array[@current_position[0]][@current_position[1]]
+    start_position = { point: @current_position, G: 0, H: 0, F: 0 }
+    @open_list << start_position
+    adjacents(@current_position)
+    @closed_list = @open_list.delete(@current_position)
+    p @open_list
+    p @closed_list
+  end  
+=begin  
+    # p @maze[@current_position[0]][@current_position[1]]
     until end_game?(step(@current_position))
       p @current_position
       @current_position = step(@current_position)
       p @current_position
       # position_first = @current_position.first
-      p @maze_array[@current_position[0]][@current_position[1]]
-      @maze_array[@current_position[0]][@current_position[1]] = "X"
+      p @maze[@current_position[0]][@current_position[1]]
+      @maze[@current_position[0]][@current_position[1]] = "X"
       self.print
     end
     self.print
   end
-
 end
-
+=end
+end
 
 maze = Maze.new
 # p maze.print
@@ -104,4 +131,5 @@ maze = Maze.new
 # p maze.valid_adjacents(maze.adjacents(maze.end_position))
 
 # p maze.end_game?(maze.valid_adjacents(maze.adjacents(maze.end_position)))
+p maze
 maze.run
